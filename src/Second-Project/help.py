d0a68c93b07201f6ad8449a584741649591b8e75
artahadhahd <iamarta.azadegan@gmail.com>
2023-04-16 20:27:40 +0200

    shortened the help file even more

--- src/Second-Project/help.py
+++ src/Second-Project/help.py
@@ -1,3 +1,2 @@
-#!/usr/bin/env python3
-files = ' '.join([str(x) for x in __import__("pathlib").Path('.').iterdir() if x.is_file() and __import__("re").match('.*\.c', str(x))])
+files = ' '.join(str(x) for x in __import__("pathlib").Path('.').iterdir() if x.is_file() and __import__("re").match('.*\.c', str(x)))
 with open("Makefile", "w") as f:f.write(f"all:\n\t$(CC) -o Application.out {files} -lSDL2 -lGL -I.\ndebug:\n\t$(CC) -g {files} -o Debug.out -lSDL2 -lGL -I.")
